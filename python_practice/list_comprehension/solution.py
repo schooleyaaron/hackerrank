b7213592b525e37906e15cc3caff7c4d36108036
Aaron <schooleyaaron@gmail.com>
2015-02-18 21:52:09 -0500

    Solution to list comprehensions

---
+++ python_practice/list_comprehension/solution.py
@@ -0,0 +1,7 @@
+x,y,z,n,f = int(input()),int(input()),int(input()),int(input()),[]
+for i in range(0,x+1):
+    for j in range(0,y+1):
+        for k in range(0,z+1):
+            if i + j + k != n:
+                f.append([i,j,k])
+print(f)
